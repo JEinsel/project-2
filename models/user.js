914ef67decc5442f725e93f6423b8993fc4fc411
raymondt1234 <raymondt1234@gmail.com>
2019-12-03 18:49:52 -0700

    changed memberLvl default to 0 instead of 1

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
       },
       memberLvl: {
         type: DataTypes.INTEGER,
-        defaultValue: 1
+        defaultValue: 0
       }
     },
     {
