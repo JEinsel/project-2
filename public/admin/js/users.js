c7818d0648c7f25d4e6473cae177c4f66bdcd48d
Maimell <malinka77@gmail.com>
2019-12-03 23:52:20 -0700

    added cancel buttons on edit screens, changed the way to connect to db

--- public/admin/js/users.js
+++ public/admin/js/users.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
     `);
     $("#save_btn_placeholder").html(
       // eslint-disable-next-line quotes
-      `<button class="btn btn-block btn-dark" id="user_save">Save</button>`
+      `<button class="btn btn-dark" id="user_save">Save</button>
+      <button class="btn btn-danger" id="cancel">Cancel</button>`
     );
   });
 
